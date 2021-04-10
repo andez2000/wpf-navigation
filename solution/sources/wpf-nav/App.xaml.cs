e8bec22a9ae817c30b613da56afa7d2a38bee829
andez2000 <andez2000@hotmail.com>
2021-04-10 09:02:27 +0100

    Dynamic navigation is now working

--- solution/sources/wpf-nav/App.xaml.cs
+++ solution/sources/wpf-nav/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Navigation;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,8 +21,6 @@ namespace WpfNav
 
             Services = host.Services;
             
-            // App.Current.StartupUri = new Uri("Scenario2Window.xaml", UriKind.Absolute);
-
             AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
             {
                 Console.WriteLine(args);
@@ -34,36 +33,59 @@ namespace WpfNav
             services.AddSingleton<Scenario2Window>();
             
             services.AddAppNavigator(() => host.Services.GetService<MainWindow>()?.NavigationHost);
-            //NavigationService navigationService = null;
         }
 
-        protected override void OnNavigating(NavigatingCancelEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
-            var eNavigator = e.Navigator;
-
-            string s = "";
-            base.OnNavigating(e);
-            
-            
+            MainWindow window = host.Services.GetService<MainWindow>();
+            Debug.Assert(window != null, nameof(window) + " != null");
+            window.Show();
         }
 
-        protected override void OnLoadCompleted(NavigationEventArgs e)
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
-            base.OnLoadCompleted(e);
             
         }
+        
+        // [STAThreadAttribute]
+        // public static int Main(string[] args)
+        // {
+        //     App app = new App();
+        //
+        //     app.InitializeComponent();
+        //     app.Run();
+        //
+        //     return 0;
+        // }
 
-        protected override void OnNavigated(NavigationEventArgs e)
-        {
-            base.OnNavigated(e);
-        }
-
-        protected override void OnNavigationProgress(NavigationProgressEventArgs e)
-        {
-            base.OnNavigationProgress(e);
-        }
-
+        
         private IHost host;
         public static IServiceProvider Services;
     }
 }
+
+// protected override void OnNavigating(NavigatingCancelEventArgs e)
+// {
+//     var eNavigator = e.Navigator;
+//
+//     string s = "";
+//     base.OnNavigating(e);
+//     
+//     
+// }
+//
+// protected override void OnLoadCompleted(NavigationEventArgs e)
+// {
+//     base.OnLoadCompleted(e);
+//     
+// }
+//
+// protected override void OnNavigated(NavigationEventArgs e)
+// {
+//     base.OnNavigated(e);
+// }
+//
+// protected override void OnNavigationProgress(NavigationProgressEventArgs e)
+// {
+//     base.OnNavigationProgress(e);
+// }
