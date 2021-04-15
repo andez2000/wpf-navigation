a8e737f98f3f675d4dd1c4e5bc2853b0f68131d8
Paul Anderson <andez2000@hotmail.com>
2021-04-15 21:46:20 +0100

    out of the box simple vm

---
+++ solution/out-the-box/ViewModels/SimpleVM.cs
@@ -0,0 +1,12 @@
+﻿namespace out_the_box.ViewModels
+{
+    public class SimpleVM
+    {
+        public string Message { get; set; }
+
+        public SimpleVM()
+        {
+            Message = "Hello, from the simple vm";
+        }
+    }
+}
