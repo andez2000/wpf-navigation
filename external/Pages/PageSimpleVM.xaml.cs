9a4eafe4cff55c49b51393450daf32f3d1c1e796
Paul Anderson <andez2000@hotmail.com>
2021-04-15 22:18:11 +0100

    meh

---
+++ external/Pages/PageSimpleVM.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace out_the_box.Pages
+{
+    public partial class PageSimpleVM : Page
+    {
+        public PageSimpleVM()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- solution/out-the-box/FrameNavigationWindow.xaml.cs
+++ solution/out-the-box/FrameNavigationWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using out_the_box.Pages;
 
 namespace out_the_box
@@ -24,12 +25,32 @@ namespace out_the_box
         private void ContentPageNoVM_OnClick(object sender, RoutedEventArgs e)
         {
             NavigationFrame.Content = new PageNoVM();
-
         }
 
         private void ContentPageSimpleVM_OnClick(object sender, RoutedEventArgs e)
         {
             NavigationFrame.Content = new PageSimpleVM();
+        }
+
+        private void ExternalUriToPageNoVM_OnClick(object sender, RoutedEventArgs e)
+        {
+            NavigationFrame.Navigate(new Uri(
+                @"D:\vcs\github\andez2000\wpf-navigation\external\Pages\ExternalPageNoVM.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void ExternalUriToPageSimpleVM_OnClick(object sender, RoutedEventArgs e)
+        {
+            // this all doesnt work
+            
+            // var loadComponent = (Page)Application.LoadComponent(new Uri(
+            //     @"D:\vcs\github\andez2000\wpf-navigation\external\Pages\ExternalPageSimpleVM.xaml",
+            //     UriKind.RelativeOrAbsolute));
+
+            // cannot create the vm in 
+
+            NavigationFrame.Navigate(
+                new Uri(
+                @"D:\vcs\github\andez2000\wpf-navigation\external\Pages\ExternalPageSimpleVM.xaml", UriKind.RelativeOrAbsolute));
         }
     }
 }
