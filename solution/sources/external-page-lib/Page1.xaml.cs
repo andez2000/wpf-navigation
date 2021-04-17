4538a8d5092c90ffc4664c89e37b4f4340bfd0c3
andez2000 <andez2000@hotmail.com>
2021-04-17 11:07:10 +0100

    added external-page-lib.csproj

---
+++ solution/sources/external-page-lib/Page1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace acme.external
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
