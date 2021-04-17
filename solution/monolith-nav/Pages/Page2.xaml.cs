aa67bd2f13ae27ecf41d850f1b618fe9a2828ad6
andez2000 <andez2000@hotmail.com>
2021-04-17 11:43:26 +0100

    Prepping for the monolith project

---
+++ solution/monolith-nav/Pages/Page2.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace acme.monolith.Pages
+{
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
