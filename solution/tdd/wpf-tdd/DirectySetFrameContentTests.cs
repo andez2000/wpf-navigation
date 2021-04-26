543dde0012c7c52b783b5bb29859b9316904c6df
andez2000 <andez2000@hotmail.com>
2021-04-26 09:27:08 +0100

    split test to DirectySetFrameContentTests

---
+++ solution/tdd/wpf-tdd/DirectySetFrameContentTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using acme.external.Pages;
+using Xunit;
+
+namespace wpftdd
+{
+    /// <summary>
+    /// Tests around WPF and setting content with assertions to test out dispatching in tests only.
+    /// </summary>
+    public sealed class DirectySetFrameContentTests
+    {
+        [WpfFact]
+        public void JustSetFrameContent2()
+        {
+            var runTestMonitor = new ManualResetEventSlim(false);
+            var assertionsMonitor = new ManualResetEventSlim(false);
+            var windowClosedMonitor = new ManualResetEventSlim(false);
+
+            var (thread, mainWindow) = WindowDispatch.CreateWindowOnSTAThread(() => new MainWindow(), w => { });
+
+            WindowDispatch.DispatchOn(mainWindow, () =>
+            {
+                var page1 = new Page1();
+                mainWindow.NavigationHost.Content = page1;
+            }, runTestMonitor, TimeSpan.FromSeconds(1));
+
+            WindowDispatch.DispatchOn(mainWindow, () => Assert.NotNull(mainWindow.NavigationHost.Content),
+                assertionsMonitor, TimeSpan.FromSeconds(1));
+            WindowDispatch.DispatchOn(mainWindow, () => mainWindow.Close(), windowClosedMonitor,
+                TimeSpan.FromSeconds(1));
+        }
+    }
+}
--- solution/tdd/wpf-tdd/SetupModuleTdd.cs
+++ solution/tdd/wpf-tdd/SetupModuleTdd.cs
@@ -20,49 +20,30 @@ namespace wpftdd
         private readonly NamedRoute _page1Route = new("Page1", typeof(Page1));
         private readonly NamedRoute _page2Route = new("Page2", typeof(Page2WithVm));
         private readonly NamedRoute _page3Route = new("Page3", typeof(Page3WithVm));
-        
-        [WpfFact]
-        public void JustSetFrameContent2()
-        {
-            var runTestMonitor = new ManualResetEventSlim(false);
-            var assertionsMonitor = new ManualResetEventSlim(false);
-            var windowClosedMonitor = new ManualResetEventSlim(false);
-
-            var (thread, mainWindow) = WindowDispatch.CreateWindowOnSTAThread(() => new MainWindow(), w => { });
-
-            WindowDispatch.DispatchOn(mainWindow, () =>
-            {
-                var page1 = new Page1();
-                mainWindow.NavigationHost.Content = page1;
-            }, runTestMonitor, TimeSpan.FromSeconds(1));
-            
-            WindowDispatch.DispatchOn(mainWindow, () => Assert.NotNull(mainWindow.NavigationHost.Content), assertionsMonitor, TimeSpan.FromSeconds(1));
-            WindowDispatch.DispatchOn(mainWindow, () => mainWindow.Close(), windowClosedMonitor, TimeSpan.FromSeconds(1));
-        }
 
         [UIFact]
         public void Can_navigate_to_different_pages()
         {
-            (Thread Thread, MainWindow mainWindow) context = new (null, null);
-            
+            (Thread Thread, MainWindow mainWindow) context = new(null, null);
+
             // configuration
             Routes routes = new Routes();
             routes.AddAll(_page1Route, _page2Route, _page3Route);
-        
+
             Views views = new Views();
             views.Register<Page2WithVm, Page2Vm>((view, dataContext) => view.DataContext = dataContext);
             views.RegisterForAutoDataContext<Page3WithVm, Page3Vm>();
             // what to do with page1
-        
+
             // resolution
             IServiceProvider serviceProvider = null;
-            
+
             NamedRouteResolver namedRouteResolver = new(routes);
             RouteResolver routeResolver = new(namedRouteResolver);
             ViewResolver viewResolver = new ViewResolver(views, type => serviceProvider.GetService(type));
             NavigationController navigationController = new(() => context.mainWindow.NavigationHost.NavigationService);
             RouteNavigationService routeNavigationService = new(routeResolver, viewResolver, navigationController);
-        
+
             // add all routes to the container...  what is the scope for each view :?
             // wire up for DI
             ServiceCollection serviceCollection = new();
@@ -71,7 +52,7 @@ namespace wpftdd
             serviceCollection.AddScoped<Page2Vm>();
             serviceCollection.AddScoped<Page3WithVm>();
             serviceCollection.AddScoped<Page3Vm>();
-        
+
             ContainerBuilder containerBuilder = new();
             containerBuilder.Populate(serviceCollection);
             IContainer container = containerBuilder.Build();
@@ -80,15 +61,15 @@ namespace wpftdd
             {
                 var runTestMonitor = new ManualResetEventSlim(false);
                 var assertionsMonitor = new ManualResetEventSlim(false);
-                
+
                 context = WindowDispatch.CreateWindowOnSTAThread(() => new MainWindow(), w => { });
 
                 WindowDispatch.DispatchOn(
-                    context.mainWindow, () => routeNavigationService.NavigateTo(Named("Page2")), 
+                    context.mainWindow, () => routeNavigationService.NavigateTo(Named("Page2")),
                     runTestMonitor, TimeSpan.FromSeconds(3));
-                
+
                 Thread.Sleep(1000);
-                
+
                 Assert.Equal(
                     WindowDispatch.GetProperty(context.mainWindow, () => context.mainWindow.NavigationHost.Content),
                     serviceProvider.GetService<Page2WithVm>()
@@ -96,15 +77,15 @@ namespace wpftdd
 
                 // we need sleeps between navigation to give ui chance to update :)...
                 Thread.Sleep(1000);
-                WindowDispatch.DispatchOn(context.mainWindow, () =>
-                {
-                    routeNavigationService.NavigateTo(Named("Page3"));
-                }, runTestMonitor, TimeSpan.FromSeconds(1));
-                
+                WindowDispatch.DispatchOn(context.mainWindow,
+                    () => { routeNavigationService.NavigateTo(Named("Page3")); }, runTestMonitor,
+                    TimeSpan.FromSeconds(1));
+
                 Thread.Sleep(1000);
 
 
-                var property = WindowDispatch.GetProperty(context.mainWindow, () => context.mainWindow.NavigationHost.Content);
+                var property = WindowDispatch.GetProperty(context.mainWindow,
+                    () => context.mainWindow.NavigationHost.Content);
                 Assert.Equal(
                     property,
                     serviceProvider.GetService<Page3WithVm>()
@@ -113,7 +94,5 @@ namespace wpftdd
                 context.mainWindow?.Dispatcher.BeginInvoke(new ThreadStart(() => context.mainWindow.Close()));
             }
         }
-        
-        
     }
 }
